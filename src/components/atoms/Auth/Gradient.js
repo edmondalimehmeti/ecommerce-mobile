c27cef2c9019a84b5a7f2ddcad78c4682f088814
Edmond Alimehmeti <edi.alimehmeti@gmail.com>
2024-10-08 11:06:33 +0200

    fixed gradient

--- src/components/atoms/Auth/Gradient.js
+++ src/components/atoms/Auth/Gradient.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {colors} from '_theme/index';
 import LinearGradient from 'react-native-linear-gradient';
+import {SafeAreaView} from 'react-native';
 
 const Gradient = ({children, style}) => {
   return (
     <LinearGradient
       colors={[colors.lightGreen, colors.white]}
       start={{x: 0, y: 0.1}}
-      style={[{flex: 1}, style]}
+      style={[{flex: 1}]}
       end={{x: 0, y: 0.8}}>
-      {children}
+      <SafeAreaView style={[{flex: 1}, style]}>{children}</SafeAreaView>
     </LinearGradient>
   );
 };
