fab69d7b7cbd24717cc8c6c260723bfbd8e73adc
Edmond Alimehmeti <edi.alimehmeti@gmail.com>
2024-10-08 11:03:33 +0200

    fixed login screen

--- src/scenes/Auth/Login.js
+++ src/scenes/Auth/Login.js
@@ -1,16 +1,42 @@
 import React, {useState} from 'react';
 import {Screen} from '_scenes/base';
 import {colors} from '_theme/index';
-import {StyleSheet, Text, View} from 'react-native';
+import {Keyboard, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Banner from '_assets/svg/banner.svg';
 import {CButton, CInput, CPasswordInput, CText} from '_components/index';
 import Gradient from '_components/atoms/Auth/Gradient';
+import useAPI from '_utils/hooks/useAPI';
+import {useDispatch} from 'react-redux';
+import {authentication} from '_redux/app/actions';
+import {handleRequestErrors} from '_utils/helpers/functions';
+import CBack from '_components/chore/back';
+import CKeyboardAvoidingView from '_components/chore/keyboardAvoidingView';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const LoginScreen = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const {makeRequest} = useAPI();
+  const dispatch = useDispatch();
+  const {top} = useSafeAreaInsets();
 
-  const login = () => {};
+  const login = async () => {
+    Keyboard.dismiss();
+    setLoading(true);
+    try {
+      const res = await makeRequest('POST', '/auth/login', {
+        email,
+        password,
+      });
+      // dispatch(authentication(res));
+      console.log(res);
+    } catch (e) {
+      handleRequestErrors(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const goToRegister = () => {
     navigation.replace('Register');
@@ -18,23 +44,36 @@ const LoginScreen = ({navigation}) => {
 
   return (
     <Screen>
-      <Gradient style={styles.container}>
-        <Banner />
-        <CText txt="WELCOME TO DOLLAP" />
-        <View style={styles.inputContainer}>
-          <CInput value={email} onChangeText={setEmail} placeholder="EMAIL" />
-          <CPasswordInput
-            value={password}
-            onChangeText={setPassword}
-            placeholder="PASSWORD"
-          />
-          <CButton text="LOGIN" onPress={login} />
-        </View>
-        <CText txt="FORGOT PASSWORD" />
-        <CText style={{fontSize: 12}}>
-          <CText txt="DON'T HAVE AN ACCOUNT?" style={styles.text} />{' '}
-          <Text style={styles.link} children="SIGNUP" onPress={goToRegister} />
-        </CText>
+      <Gradient>
+        <CBack containerStyle={{paddingHorizontal: 20}} />
+        <CKeyboardAvoidingView containerStyle={styles.container}>
+          <Banner />
+          <CText txt="WELCOME TO DOLLAP" />
+          <View style={styles.inputContainer}>
+            <CInput
+              value={email}
+              textContentType="oneTimeCode"
+              keyboardType="email-address"
+              onChangeText={setEmail}
+              placeholder="EMAIL"
+            />
+            <CPasswordInput
+              value={password}
+              onChangeText={setPassword}
+              placeholder="PASSWORD"
+            />
+            <CButton text="LOGIN" onPress={login} />
+          </View>
+          <CText txt="FORGOT PASSWORD" />
+          <CText style={{fontSize: 12}}>
+            <CText txt="DON'T HAVE AN ACCOUNT?" style={styles.text} />{' '}
+            <Text
+              style={styles.link}
+              children="SIGNUP"
+              onPress={goToRegister}
+            />
+          </CText>
+        </CKeyboardAvoidingView>
       </Gradient>
     </Screen>
   );
